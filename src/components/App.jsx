79b4416425f7ca85fa2f9ded1958098e0b7627fe
GOOSxE <der.goose.4308@gmail.com>
2023-07-02 13:50:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,28 @@
+// *
 import React from 'react';
 import Section from './Section/Section';
 import ContactForm from './Contact-form/Contact-form';
 import Filter from './FIlter/Filter';
 import ContactsList from './Contact-list/Contact-list';
 import Notification from './Notification/Notification';
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// ? // Ключ сховища ;
+const STORAGE_KEY = 'contacts';
+// ? // Масив контактів який перевіряє наявність та отримує контакти із local storage ;
+let loadedContactsArray = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+// ? // Кмпонент App ;
 export class App extends React.Component {
   state = {
-    contacts: initialContacts,
+    contacts: loadedContactsArray,
     filter: '',
   };
+  componentDidUpdate() {
+    if (this.state.contacts !== loadedContactsArray) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
+    }
+    if (this.state.contacts.length === 0) {
+      localStorage.removeItem(STORAGE_KEY);
+    }
+  }
   onContactAdding = contactData => {
     if (
       this.state.contacts.find(contact => contact.name === contactData.name)
@@ -24,7 +32,6 @@ export class App extends React.Component {
     }
     this.setState({ contacts: [...this.state.contacts, contactData] });
   };
-
   onContactRemoving = id => {
     this.setState({
       contacts: this.state.contacts.filter(contact => contact.id !== id),
@@ -58,7 +65,7 @@ export class App extends React.Component {
               onContactRemoving={this.onContactRemoving}
             ></ContactsList>
           ) : (
-            <Notification title="There is no contacts"></Notification>
+            <Notification message="There is no contacts"></Notification>
           )}
         </Section>
       </div>
